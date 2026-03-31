abbab3b59722210d898f2fe20c137f02c5acfd8b
SayHoang <sayhoang.work@gmail.com>
2026-03-31 10:54:01 +0700

    Add Test Suites

---
+++ Test Suites/HA/TS_StockReport.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_StockReport</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>69745b22-6369-4e0f-b5fc-6e6815975960</testSuiteGuid>
+   <testCaseLink>
+      <guid>bbb52070-6980-4f94-96d7-8e9ea2bec726</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HA/TC_010_StockReport</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>18444820-26c2-423c-83e7-e80be56c0a89</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
